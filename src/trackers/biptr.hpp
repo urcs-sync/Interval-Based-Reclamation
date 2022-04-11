10c25f9af13cae8e8cce958ace674281a8a8c7b4
Haosen Wen <wenhaosen@gmail.com>
2022-04-11 02:39:52 -0400

    in biptr<T>, use std::atomic<>::compare_exchange_strong in place of asm wide CAS

--- src/trackers/biptr.hpp
+++ src/trackers/biptr.hpp
@@ -20,112 +20,37 @@ limitations under the License.
 #ifndef BIPTR_HPP
 #define BIPTR_HPP
 
-template<class T> struct fat_pointer_rec{
-	// T* ptr;
-	// uint64_t tag;
-	std::atomic<T*> ptr;
-	std::atomic<uint64_t> tag;
-	fat_pointer_rec(T* p, uint64_t t){
-		ptr.store(p, std::memory_order_release);
-		tag.store(t, std::memory_order_release);
-	}
-	fat_pointer_rec(){
-		ptr.store(nullptr, std::memory_order_release);
-		tag.store(0, std::memory_order_release);
-	}
-};
-
-template<class T> class FatPtr{
-public:
-	fat_pointer_rec<T> fat_p;
-
-// public:
-	FatPtr(T* p, uint64_t t): fat_p(p, t){}
-	FatPtr(){}
-	//inline bool WideCAS(fat_pointer_rec<T> *addr, fat_pointer_rec<T> &old_value, fat_pointer_rec<T> &new_value) {
-
-#if (__x86_64__ || __ppc64__)
-	inline bool WideCAS(fat_pointer_rec<T> &old_value, 
-		fat_pointer_rec<T> &new_value, std::memory_order morder) {
-		bool ret;
-		__asm__ __volatile__(
-		"lock cmpxchg16b %1;\n"
-		"sete %0;\n"
-		:"=m"(ret),"+m" (*(volatile fat_pointer_rec<T> *) (&fat_p))
-		:"a" (old_value.ptr), "d" (old_value.tag), "b" (new_value.ptr), "c" (new_value.tag));
-		std::atomic_thread_fence(morder);
-		return ret;
-	}
-#else
-	inline bool WideCAS(fat_pointer_rec<T> &old_value, 
-		fat_pointer_rec<T> &new_value, std::memory_order morder) {
-		errexit("WCAS not supported with -m32.");
-	}
-#endif
-
-	inline bool WideCAS(fat_pointer_rec<T> &old_value, fat_pointer_rec<T> &new_value){
-		return WideCAS(old_value, new_value, std::memory_order_release);
-	}
-	inline T* load_ptr(std::memory_order morder){
-		// std::atomic_thread_fence(morder);
-		// return fat_p.ptr;
-		return fat_p.ptr.load(morder);
-	}
-	inline T* load_ptr(){
-		return load_ptr(std::memory_order_acquire);
-	}
-	inline void store_ptr(T* &p, std::memory_order morder){
-		// fat_p.ptr = p;
-		// std::atomic_thread_fence(morder);
-		fat_p.ptr.store(p, morder);
-	}
-	inline void store_ptr(T* &p){
-		store_ptr(p, std::memory_order_release);
-	}
-	inline uint64_t load_tag(std::memory_order morder){
-		return fat_p.tag.load(morder);
-	}
-	inline uint64_t load_tag(){
-		return load_tag(std::memory_order_acquire);
-	}
-	inline void store_tag(uint64_t t, std::memory_order morder){
-		fat_p.tag.store(t, morder);
-	}	
-	inline void store_tag(uint64_t t){
-		store_tag(t, std::memory_order_release);
-	}
-};
+template<class T> struct FatPtr{
+	T* ptr;
+	uint64_t tag;
+	FatPtr(): ptr(nullptr), tag(0){}
+	FatPtr(T* p, uint64_t t) : ptr(p), tag(t) {}
+} __attribute__(( __aligned__(16)));
 
 template<class T> class biptr{
 private:
 	std::atomic<uint64_t>* birth_before;
 	std::atomic<T*>* p;
 	//FatPtr typed 128 bit record
-	FatPtr<T> fat_ptr;
+	std::atomic<FatPtr<T>> fat_ptr;
 
 public:
 	static RangeTracker<T>* range_tracker;
 
 	biptr<T>(){
-		// p->store(NULL, std::memory_order_release);
-		// birth_before->store(get_birth_epoch(NULL), std::memory_order_release);
-		birth_before = &(fat_ptr.fat_p.tag);
-		p = &(fat_ptr.fat_p.ptr);
+		birth_before = (std::atomic<uint64_t>*) ((char*) &(fat_ptr) + sizeof(T*));
+		p = (std::atomic<T*>*)((char*) &(fat_ptr));
 	}
-	biptr<T>(T* obj): fat_ptr(obj, get_birth_epoch(obj)){
-		// p->store(obj, std::memory_order_release);
-		// birth_before->store(get_birth_epoch(obj), std::memory_order_release);
-		birth_before = &(fat_ptr.fat_p.tag);
-		p = &(fat_ptr.fat_p.ptr);
+	biptr<T>(T* obj) {
+		std::atomic_init(&fat_ptr, FatPtr<T>(obj, get_birth_epoch(obj)));
+		birth_before = (std::atomic<uint64_t>*) ((char*) &(fat_ptr) + sizeof(T*));
+		p = (std::atomic<T*>*)((char*) &(fat_ptr));
 
 	}
-	biptr<T>(biptr<T> &other): fat_ptr(other.ptr(), other.birth()){
-		// if (range_tracker->type != WCAS){
-		// 	p->store(other.ptr(), std::memory_order_release);
-		// 	birth_before->store(other.birth(), std::memory_order_release);
-		// }
-		birth_before = &(fat_ptr.fat_p.tag);
-		p = &(fat_ptr.fat_p.ptr);
+	biptr<T>(biptr<T> &other) {
+		std::atomic_init(&fat_ptr, FatPtr<T>(other.ptr(), other.birth()));
+		birth_before = (std::atomic<uint64_t>*) ((char*) &(fat_ptr) + sizeof(T*));
+		p = (std::atomic<T*>*)((char*) &(fat_ptr));
 	}
 
 	static void set_tracker(RangeTracker<T>* tracker){
@@ -139,12 +64,12 @@ public:
 	inline uint64_t birth(){
 		// return range_tracker->type == WCAS? 
 		// 	fat_ptr.load_tag() : birth_before->load(std::memory_order_acquire);
-		return fat_ptr.load_tag();
+		return birth_before->load(std::memory_order_acquire);
 	}
 	inline T* ptr() {
 		// return range_tracker->type == WCAS?
 		// 	fat_ptr.load_ptr() : p->load(std::memory_order_acquire);
-		return fat_ptr.load_ptr();
+		return p->load(std::memory_order_acquire);
 	}
 	inline T* load(){
 		return ptr();
@@ -182,9 +107,9 @@ public:
 			return p->compare_exchange_strong(ori, obj, morder,
 				std::memory_order_relaxed);
 		} else { // WCAS
-			fat_pointer_rec<T> old_value(ori, fat_ptr.load_tag());
-			fat_pointer_rec<T> new_value(obj, get_birth_epoch(obj));
-			return fat_ptr.WideCAS(old_value, new_value);
+			FatPtr<T> old_value(ori, birth_before->load());
+			FatPtr<T> new_value(obj, get_birth_epoch(obj));
+			return fat_ptr.compare_exchange_strong(old_value, new_value);
 		}
 	}
 
@@ -208,9 +133,9 @@ public:
 			this->birth_before->store(get_birth_epoch(obj), std::memory_order_relaxed);
 			this->p->store(obj, std::memory_order_relaxed);
 		} else {
-			fat_pointer_rec<T> old_value(fat_ptr.load_ptr(), fat_ptr.load_tag());
-			fat_pointer_rec<T> new_value(obj, get_birth_epoch(obj));
-			while(!this->fat_ptr.WideCAS(old_value, new_value));
+			FatPtr<T> old_value(p->load(), birth_before->load());
+			FatPtr<T> new_value(obj, get_birth_epoch(obj));
+			while(!this->fat_ptr.compare_exchange_strong(old_value, new_value));
 		}
 		return *this;
 	}
@@ -224,9 +149,9 @@ public:
 				this->birth_before->store(other.birth(), std::memory_order_relaxed);
 				this->p->store(other.ptr(), std::memory_order_relaxed);
 			} else {
-				fat_pointer_rec<T> old_value(fat_ptr.load_ptr(), fat_ptr.load_tag());
-				fat_pointer_rec<T> new_value(other.ptr(), other.birth());
-				while(!this->fat_ptr.WideCAS(old_value, new_value));
+				FatPtr<T> old_value(p->load(), birth_before->load());
+				FatPtr<T> new_value(other.ptr(), other.birth());
+				while(!this->fat_ptr.compare_exchange_strong(old_value, new_value));
 			}
 		}
 		return *this;
